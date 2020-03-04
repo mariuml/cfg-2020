7d8d5ebc12bae30bbcc1c0273bfefbae53ffc849
mariuml <60156362+mariuml@users.noreply.github.com>
2020-03-04 19:01:21 +0000

    CodeFirst Girls

---
+++ CodeFirst Girls/Week 4 - Javascript/Homework game/Homework game/The Game.js
@@ -0,0 +1,6 @@
+var puzzle1 = prompt("Welcome to The Maze! In order to escape, you have to correctly solve the riddles and puzzles. Are you ready to play?")
+if(puzzle1 == "yes") {
+    alert( "Great!")
+} else if (puzzle1 == "no") {
+    alert ("You're missing out!")
+} else { alert("I didn't understand, please refresh the page and try again")}
